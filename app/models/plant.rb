ef4e192997e8521441007715bf3e72d43056da02
loic redon <jabemalo30@gmail.com>
2020-06-09 15:35:39 +0200

    regex url

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,5 +2,5 @@ class Plant < ApplicationRecord
   belongs_to :garden
 
   validates :name, presence: true, uniqueness: { scope: :garden_id }
-  validates :image_url, presence: true
+  validates :image_url, presence: true, format: { with: /(https?:\/\/.*\.(?:png|jpg))/i, message: "please enter an image url" }
 end
